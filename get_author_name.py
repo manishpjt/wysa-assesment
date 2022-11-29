f69e0606b5712748ebae417df396230de3cf86f7
Manish Prajapati <110806066+manishpjt@users.noreply.github.com>
2022-11-29 20:27:01 +0000

    Added python file to get username

---
+++ get_author_name.py
@@ -0,0 +1,9 @@
+import json
+import requests
+import os
+
+pr_number = int(os.enviorn["PR_NUMBER"])
+
+r = requests.get(f"https://api.github.com/repos/manishpjt/wysa-assesment/pulls/{pr_number}/commits")
+result = json.loads(r.text)[0]
+print(result["commit"]["author"]["name"])
